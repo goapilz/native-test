89c5e6e2d8c139f9738978fd2f458743cebf8e6b
goapilz <markus_29@gmx.de>
2017-11-29 21:24:35 +0100

    addentry

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import React from 'react'
+import {View, Text} from 'react-native'
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>xxx233 Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shakex your phone to open the developer menu.</Text>
-	<Ionicons name='ios-pizza' color='red' size={180}/>
-      </View>
-    );
-  }
-}
+    componentDidMount() {
+        console.log('before')
+        debugger
+        console.log('after')
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    render() {
+        return (
+            <View>
+                <Text>123</Text>
+                <AddEntry/>
+                <AddEntry/><AddEntry/><AddEntry/><AddEntry/><AddEntry/>
+
+            </View>
+        )
+    }
+}
