7e8d4476bc4ce6dfafbcacdb04959d06c4b789d6
goapilz <markus_29@gmx.de>
2017-12-02 12:19:05 +0100

    flex layout

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import {StyleSheet, Text, View, AppRegistry } from 'react-native'
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: 50,
+        margin: 20,
         backgroundColor: '#00f',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     box: {
         height: 50,
@@ -19,11 +22,9 @@ class FlexboxExamples extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.box}/>
-                <View style={styles.box}/>
-                <View style={styles.box}/>
-                <View style={styles.box}/>
-                <View style={styles.box}/>
+                <View style={[styles.box, {flex: 1}]}/>
+                <View style={[styles.box, {flex: 2}]}/>
+                <View style={[styles.box, {flex: 1}]}/>
             </View>
         )
     }
