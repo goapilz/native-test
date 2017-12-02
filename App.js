bc1f0e47f1c7ec290b80fdb0a7e951300578cc0b
goapilz <markus_29@gmx.de>
2017-12-02 14:46:15 +0100

    added calendar/history

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import {View, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import AddEntry from './components/AddEntry'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
+import {Dimensions} from 'react-native';
+import History from './components/History'
 
 export default class App extends React.Component {
     componentDidMount() {
@@ -13,10 +15,12 @@ export default class App extends React.Component {
     }
 
     render() {
+        const {width, height} = Dimensions.get('window');
         return (
             <Provider store={createStore(reducer)}>
                 <View style={{flex: 1}}>
-                    <AddEntry/>
+                    <Text>{width}*{height}</Text>
+                    <History/>
                 </View>
             </Provider>
         )
