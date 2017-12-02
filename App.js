b3786bc7099c8c22bb06f113e3404b2f866c6bf3
goapilz <markus_29@gmx.de>
2017-12-02 16:27:21 +0100

    tabs

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Platform} from 'react-native'
 import AddEntry from './components/AddEntry'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
-import {Dimensions} from 'react-native';
 import History from './components/History'
+import {TabNavigator} from 'react-navigation'
+import {purple, white} from './utils/colors'
+import {FontAwesome, Ionicons} from '@expo/vector-icons'
+
+const Tabs = TabNavigator({
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: 'History',
+            tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+        },
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+        },
+    },
+}, {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === 'ios' ? white : purple,
+            shadowColor: 'rgba(0, 0, 0, 0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+})
 
 export default class App extends React.Component {
+
     componentDidMount() {
         console.log('before')
         debugger
@@ -15,12 +53,10 @@ export default class App extends React.Component {
     }
 
     render() {
-        const {width, height} = Dimensions.get('window');
         return (
             <Provider store={createStore(reducer)}>
                 <View style={{flex: 1}}>
-                    <Text>{width}*{height}</Text>
-                    <History/>
+                    <Tabs/>
                 </View>
             </Provider>
         )
