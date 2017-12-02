b51c28e345986d1f4522c3fff13ff1eeb504955f
goapilz <markus_29@gmx.de>
2017-12-02 15:14:22 +0100

    history and loading

--- components/History.js
+++ components/History.js
@@ -7,8 +7,15 @@ import {fetchCalendarResults} from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
 import {white} from '../utils/colors'
 import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
+import {AppLoading} from 'expo'
 
 class History extends Component {
+
+    state = {
+        ready: false
+    }
+
     componentDidMount() {
         const {dispatch} = this.props
 
@@ -27,9 +34,17 @@ class History extends Component {
     renderItem = ({today, ...metrics}, formattedDate, key) => (
         <View style={styles.item}>
             {today
-                ? <View><DateHeader date={formattedDate}/><Text style={styles.noDataText}>{today}</Text></View>
+                ? <View>
+                    <DateHeader date={formattedDate}/>
+                    <Text style={styles.noDataText}>
+                        {today}
+                    </Text>
+                </View>
                 : <TouchableOpacity
-                    onPress={() => console.log('Pressed!')}><Text>{JSON.stringify(metrics)}</Text></TouchableOpacity>}
+                    onPress={() => console.log('Pressed!')}
+                >
+                    <MetricCard date={formattedDate} metrics={metrics}/>
+                </TouchableOpacity>}
         </View>
     )
 
@@ -46,6 +61,10 @@ class History extends Component {
 
     render() {
         const {entries} = this.props
+        const {ready} = this.state
+        if (ready === false) {
+            return <AppLoading/>
+        }
         return (
             <UdaciFitnessCalendar
                 items={entries}
